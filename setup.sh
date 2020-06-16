ad9c927613eb6a66f8c695fad8e32659fdb0f348
Melinda Ring <MelsAir2@MacBook-Air-6.local>
2020-06-15 22:01:45 -0500

    Adding zsh config files

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# brew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew tap homebrew/cask-fonts
+brew cask install font-meslolg-nerd-font
+
+# meslolgmdz nerd font
+
+brew install starship
+brew install exa
+brew install git
+
+
+# mv -S .jbak -b
+
+# wget https://raw.githubusercontent.com/nathanbuchar/atom-one-dark-terminal/master/scheme/iterm/One%20Dark.itermcolors
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+zinit self-update
